cd0d0ea40f76df7475243f260d9a01dcf7583d19
Daniil331 <daniilasaev04493@gmail.com>
2023-04-24 21:47:10 +0300

    HM

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -3,16 +3,15 @@ from .models import Tag, Scope, Article
 from django.core.exceptions import ValidationError
 from django.forms import BaseInlineFormSet
 
-# class ScopeInlineFormset(BaseInlineFormSet):
-#     def clean(self):
-#         for form in self.forms:
-#             # form.cleaned_data
-#             # print(form.cleaned_data)
-#             if form.cleaned_data['is_main'] is True:
-#             #      raise ValidationError('Тут всегда ошибка')
-#                 raise ValidationError('Тут всегда ошибка')
-#         return super().clean()
 
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        for form in self.forms:
+            form.cleaned_data
+            print(form.cleaned_data)
+            if form.cleaned_data['is_main'] is True:
+                raise ValidationError('Тут всегда ошибка')
+        return super().clean()
 
 
 class ScopeInline(admin.TabularInline):
@@ -20,11 +19,12 @@ class ScopeInline(admin.TabularInline):
     extre = 0
     #formset = ScopeInlineFormset
 
+
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    tage_name = ['id','name']
+    tage_name = ['id', 'name']
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-       inlines = [ScopeInline]
+    inlines = [ScopeInline]
