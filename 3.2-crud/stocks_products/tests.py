f9dbe025922568f121ea976c475ffe2618a92ad8
Daniil3311 <123207839+Daniil3311@users.noreply.github.com>
2023-06-03 20:32:23 +0300

    Update tests.py

--- 3.2-crud/stocks_products/tests.py
+++ 3.2-crud/stocks_products/tests.py
@@ -2,7 +2,13 @@ from unittest import TestCase
 from rest_framework.test import APIClient
 
 
-import os.environ.setdefault ('DJANGO_SETTINGS_MODULE', 'stocks_products.settings')
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stocks_products.settings')
+
+application = get_wsgi_application()
 import django
 django.setup()
 
