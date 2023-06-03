ac30d7204c329cd833ca3bd33efb17b09e0391f4
Daniil3311 <daniil@89-108-64-173.cloudvps.regruhosting.ru>
2023-06-03 17:40:45 +0300

    added deploy.sh

---
+++ 3.2-crud/stocks_products/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /home/daniil/Processing-requests-and-templates/3.2-crud/stocks_products
+git pull origin ci
+source env/bin/activate
+python manage.py migrate
+sudo systemctl restart gunicorn
