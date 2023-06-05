33b51989f4db779dd51273dff5b71950fc99d96d
Daniil3311 <123207839+Daniil3311@users.noreply.github.com>
2023-06-05 16:17:26 +0300

    Update deploy.sh

--- 3.2-crud/stocks_products/deploy.sh
+++ 3.2-crud/stocks_products/deploy.sh
@@ -2,5 +2,5 @@
 cd /home/daniil/Processing-requests-and-templates/3.2-crud/stocks_products
 git pull origin ci
 source env/bin/activate
-python manage.py
+python manage.py migrate
 sudo systemctl restart gunicorn
