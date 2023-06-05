e9bd9b2d2cfb9a6003e2563b25f7587419209108
Daniil3311 <123207839+Daniil3311@users.noreply.github.com>
2023-06-05 17:07:07 +0300

    Update deploy.sh

--- 3.2-crud/stocks_products/deploy.sh
+++ 3.2-crud/stocks_products/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /home/daniil/Processing-requests-and-templates/3.2-crud/stocks_products
+export DJANGO_SETTINGS_MODULE=stocks_products.settings
 git pull origin ci
 source env/bin/activate
 python manage.py migrate
